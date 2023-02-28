eb734c9b011c67d133584653ce55c5d539cf84fb
Adryan Mendes <adryanmendes2014@gmail.com>
2023-02-28 19:41:24 -0300

    feature:registrando urls no localStorage
    recriação do código para que as urls geradas sejam mantidas na página mesmo após a atualizar, armazenamento no localStorage

--- js/main.js
+++ js/main.js
@@ -23,113 +23,7 @@ hamburguerIcon.addEventListener("click", () => {
     }
 });
 
-
-//codigo para formulario
-const form = document.getElementById("form");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    encurtarUrl();
-
-    setTimeout(() => {
-        form.reset();
-    }, 1500);
-
-})
-
-function encurtarUrl() {
-    //validar se a url existe
-    let url = document.getElementById("input-url");
-    let labelUrl = document.getElementById("label-url");
-    try {
-        let urlValida = new URL(url.value)
-        console.log("Valid URL!")
-        if(url.classList.contains("main-section-two-short-form-label-input-error") && labelUrl.classList.contains("main-section-two-short-form-label-error")){
-            url.classList.remove("main-section-two-short-form-label-input-error");
-            labelUrl.classList.remove("main-section-two-short-form-label-error")
-            }
-    } catch (err) {
-        console.log("Invalid URL!")
-        if(!url.classList.contains("main-section-two-short-form-label-input-error") && !labelUrl.classList.contains("main-section-two-short-form-labelerror")){
-        url.classList.add("main-section-two-short-form-label-input-error");
-        labelUrl.classList.add("main-section-two-short-form-label-error")
-        }
-        return
-    }
-
-
-    //encurtador de links
-    let headers = {
-        "Content-Type": "application/json",
-        "apikey": "81924584c3c14052a125a1505b465a4a",
-    }
-
-    let linkRequest = {
-        destination: document.getElementById("input-url").value,
-        domain: { fullName: "rebrand.ly" }
-    }
-
-    fetch("https://api.rebrandly.com/v1/links", {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify(linkRequest)
-    })
-        .then(response => response.json())
-        .then(json => {
-            /*let input = document.getElementById("input-url");
-            input.value = json.shortUrl;*/
-
-            let urlsList = document.getElementById("main-section-two-shortened");
-
-            let urlBeforeShorten = document.createElement("li");
-            urlBeforeShorten.classList.add("main-section-two-shortened-items-current")
-            urlBeforeShorten.innerText = document.getElementById("input-url").value;
-
-            let urlShortened = document.createElement("li");
-            urlShortened.classList.add("main-section-two-shortened-items-shortened")
-            urlShortened.innerText = json.shortUrl;
-
-            let linkDivCopyUrl = document.createElement("a");
-            linkDivCopyUrl.classList.add("main-section-two-shortened-items-link");
-
-            let buttonCopyUrl = document.createElement("button");
-            buttonCopyUrl.classList.add("main-section-two-shortened-items-link-button");
-            /*buttonCopyUrl.setAttribute("onclick","copyShortenedLink()");*/
-            buttonCopyUrl.innerText = "Copy";
-
-            let divUrl = document.createElement("ul");
-            divUrl.classList.add("main-section-two-shortened-items");
-
-            linkDivCopyUrl.appendChild(buttonCopyUrl)
-
-            divUrl.appendChild(urlBeforeShorten);
-            divUrl.appendChild(urlShortened);
-            divUrl.appendChild(linkDivCopyUrl);
-
-            urlsList.appendChild(divUrl);
-
-            const buttonsCopy = document.querySelectorAll(".main-section-two-shortened-items-link-button");
-
-            buttonsCopy.forEach((button) => {
-                button.addEventListener("click", (event) => {
-                    let clickedButtonDiv = event.target;
-                    let shortenedUrlCopy = clickedButtonDiv.parentNode.parentNode.children[1].innerText;
-                    
-                    navigator.clipboard.writeText(shortenedUrlCopy);
-                    button.innerText = "Copied!"
-                    button.style.background = "#3b3054";
-                })
-            })
-
-        });
-}
-
-
-
-
-
-
+//codigo para esconder o header quando o user rola pra baixo e quando rola pra cima ele aparecer
 let lastScrollTop = 0;
 
 window.addEventListener('scroll', function (e) {
@@ -144,3 +38,144 @@ window.addEventListener('scroll', function (e) {
     lastScrollTop = this.scrollY;
 })
 
+//codigo para envio do formulario
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    shortenUrl();
+
+    setTimeout(() => {
+        form.reset();
+    }, 1500);
+
+})
+
+//codigo para renderizar elementos no html
+const urls = JSON.parse(localStorage.getItem("urls")) || [];
+
+urls.forEach((url) => {
+    createElement(url)
+})
+
+//função para encurtar as urls
+function shortenUrl() {
+    //validar se a url existe
+    let url = document.getElementById("input-url");
+    let labelUrl = document.getElementById("label-url");
+    // Usando a API URL para validar a URL
+    let urlValida = false;
+    try {
+        urlValida = Boolean(new URL(url.value));
+    } catch (err) {
+        // URL inválida
+    }
+
+    if (!urlValida) {
+        console.log("Invalid URL!");
+        addErrorClass(url, labelUrl);
+        return;
+    }
+
+    console.log("Valid URL!");
+    removeErrorClass(url, labelUrl);
+
+
+    //encurtador de links
+    let headers = {
+        "Content-Type": "application/json",
+        "apikey": "81924584c3c14052a125a1505b465a4a",
+    }
+
+    let linkRequest = {
+        destination: url.value,
+        domain: { fullName: "rebrand.ly" }
+    }
+
+    fetch("https://api.rebrandly.com/v1/links", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(linkRequest)
+    })
+        .then(response => response.json())
+        .then(json => {
+            let urlBeforeShorten = url.value;
+            let urlShortened = json.shortUrl;
+
+            //cria um objeto contendo como chaves as urls antes de encurtar e após encurtar
+            const pairOfUrls = {
+                "urlBeforeShorten": urlBeforeShorten,
+                "shortenedUrl": urlShortened
+            }
+
+            urls.push(pairOfUrls);
+
+            localStorage.setItem("urls", JSON.stringify(urls));
+            createElement(pairOfUrls);
+        });
+
+}
+
+//funções para adicionar ou remover a classe erro caso não seja enviado uma URL válida
+function addErrorClass(inputElement, labelElement) {
+    if (!inputElement.classList.contains("main-section-two-short-form-label-input-error") && !labelElement.classList.contains("main-section-two-short-form-label-error")) {
+        inputElement.classList.add("main-section-two-short-form-label-input-error");
+        labelElement.classList.add("main-section-two-short-form-label-error")
+    }
+}
+
+function removeErrorClass(inputElement, labelElement) {
+    if (inputElement.classList.contains("main-section-two-short-form-label-input-error") && labelElement.classList.contains("main-section-two-short-form-label-error")) {
+        inputElement.classList.remove("main-section-two-short-form-label-input-error");
+        labelElement.classList.remove("main-section-two-short-form-label-error")
+    }
+}
+
+//função para criar um novo elemento HTML contendo a url encurtada e a url antes de encurtar
+function createElement(item) {
+    let urlsList = document.getElementById("main-section-two-shortened");
+
+    let urlBeforeShortenElement = document.createElement("li");
+    urlBeforeShortenElement.classList.add("main-section-two-shortened-items-current")
+    urlBeforeShortenElement.innerText = item.urlBeforeShorten;
+
+    let urlShortenedElement = document.createElement("li");
+    urlShortenedElement.classList.add("main-section-two-shortened-items-shortened")
+    urlShortenedElement.innerText = item.shortenedUrl;
+
+    let linkDivCopyUrl = document.createElement("a");
+    linkDivCopyUrl.classList.add("main-section-two-shortened-items-link");
+
+    let buttonCopyUrl = document.createElement("button");
+    buttonCopyUrl.classList.add("main-section-two-shortened-items-link-button");
+    buttonCopyUrl.innerText = "Copy";
+
+    let divUrl = document.createElement("ul");
+    divUrl.classList.add("main-section-two-shortened-items");
+
+    linkDivCopyUrl.appendChild(buttonCopyUrl)
+
+    divUrl.appendChild(urlBeforeShortenElement);
+    divUrl.appendChild(urlShortenedElement);
+    divUrl.appendChild(linkDivCopyUrl);
+
+    urlsList.appendChild(divUrl);
+
+    const buttonsCopy = document.querySelectorAll(".main-section-two-shortened-items-link-button");
+
+    buttonsCopy.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            let clickedButtonDiv = event.target;
+            let shortenedUrlCopy = clickedButtonDiv.parentNode.parentNode.children[1].innerText;
+
+            navigator.clipboard.writeText(shortenedUrlCopy);
+            button.innerText = "Copied!"
+            button.style.background = "#3b3054";
+
+            setTimeout(() => {
+                button.innerText = "Copy"
+                button.style.background = "#2acfcf";
+            }, 3000)
+        })
+    });
+}
